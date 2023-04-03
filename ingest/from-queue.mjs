b63ec7baba31a55b49c4fbf45a76ee61f813e801
Jonathan Perry <jonathanperry@sequencemedia.net>
2023-04-03 18:58:59 +0100

    Removes `s3:TestEvent` messages from queue

--- ingest/from-queue.mjs
+++ ingest/from-queue.mjs
@@ -4,8 +4,25 @@
 
 import PubSub from 'pubsub-js'
 
+import dispatchSQSDeleteMessage from '#utils/dispatch-sqs-delete-message'
 import genSQSReceiveMessage from '#utils/gen-sqs-receive-message'
 
+/**
+ *  Test events are put onto the queue by AWS
+ */
+PubSub.subscribe('aws:sqs:message', async (topic, message) => {
+  if (isS3TestEvent(message)) await dispatchSQSDeleteMessage(message)
+})
+
+function isS3TestEvent ({ Body: body }) {
+  const {
+    Event: event
+  } = JSON.parse(body)
+
+  return (
+    event === 's3:TestEvent'
+  )
+}
 /**
  * Initiates the generator iterator
  *
